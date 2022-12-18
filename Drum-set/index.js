44de63808ba099bd8f1f3691ff721001e6bb61e5
ADITYA0714 <95866052+ADITYA0714@users.noreply.github.com>
2022-12-18 17:57:52 +0530

    Add files via upload

---
+++ Drum-set/index.js
@@ -0,0 +1,51 @@
+var i;
+for (i = 0; i < 7; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+       var clickedkey = this.innerHTML;
+       makeSound(clickedkey);
+    })   
+}
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+})
+
+function makeSound(key){
+    switch(key){
+        case"w":
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        break;
+
+        case"a":
+        var audio = new Audio("sounds/kick.mp3");
+        audio.play();
+        break;
+
+        case"s":
+        var audio = new Audio("sounds/snare.mp3");
+        audio.play();
+        break;
+
+        case"d":
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        break;
+        
+        case"j": 
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+
+        case"k":
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+        break;
+        
+        case"l": 
+        var audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+        break;
+       }
+    };
+
